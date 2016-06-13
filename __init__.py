1f54341ed39934ee232264bd4ea80c2587985433
pushebp <pushebp@163.com>
2016-06-13 14:51:02 +0800

    提供少数封装的函数

--- __init__.py
+++ __init__.py
@@ -230,11 +230,13 @@ def postdata2():
     print post_data
     cj = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    headers = {'Host':'passport.jiayuan.com',\
-                'User-Agent':'Mozilla/5.0 (Windows NT 6.3; rv:35.0) Gecko/20100101 Firefox/35.0',\
-                'Accept':'*/*',\
-                'Accept-Language':'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',\
-                'Accept-Encoding':'gzip, deflate'}
+    headers = {
+        'Host':'passport.jiayuan.com',\
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.3; rv:35.0) Gecko/20100101 Firefox/35.0',\
+        'Accept':'*/*',\
+        'Accept-Language':'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',\
+        'Accept-Encoding':'gzip, deflate'
+    }
     website = 'https://passport.jiayuan.com/dologin.php?pre_url=http://www.jiayuan.com/usercp'
     req = urllib2.Request(website, post_data, headers)
     response = opener.open(req)
